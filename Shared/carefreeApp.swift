53800d493b4af98e8eabb569ad19458b4d543d1e
dvdprz <51711570+dvdprz@users.noreply.github.com>
2021-11-01 08:49:37 -0500

    Initial Commit

---
+++ Shared/carefreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  carefreeApp.swift
+//  Shared
+//
+//  Created by DEV on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct carefreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
